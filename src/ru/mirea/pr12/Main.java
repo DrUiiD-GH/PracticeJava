996ec22aa4fe03056588e5c54a6c531e4caf83cf
DrUiiD <brusov2001@bk.ru>
2020-10-26 22:36:15 +0300

    pr12
    beta 1

--- src/ru/mirea/pr12/Main.java
+++ src/ru/mirea/pr12/Main.java
@@ -7,30 +7,54 @@ import javafx.geometry.Pos;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginPage extends Application {
+public class Main extends Application {
+    private int accumulator1, accumulator2, numberOfOperation;
+
+    private boolean checkInt(String str){
+        try{
+            Integer.parseInt(str);
+            return true;
+        }catch (NumberFormatException e){return false;}
+    }
+
+    private String signOfOperation(int num){
+     switch (num){
+         case 1: return " + ";
+         case 2: return " - ";
+         case 3: return " * ";
+         case 4: return " / ";
+         default: return "";
+     }
+    }
+
+    private int operationByNumber(int num){
+
+    }
+
+
+
     @Override
     public void start(Stage stage) {
-        //creating label email
-        Text text1 = new Text("Email");
 
-        //creating label password
-        Text text2 = new Text("Password");
 
+
+        Text expression = new Text();
         //Creating Text Filed for email
         TextField textField1 = new TextField();
 
-        //Creating Text Filed for password
-        PasswordField textField2 = new PasswordField();
 
         //Creating Buttons
-        Button button1 = new Button("Submit");
-        Button button2 = new Button("Clear");
+        Button buttonPlus = new Button("+");
+        Button buttonMinus = new Button("-");
+        Button buttonMultiplication = new Button("*");
+        Button buttonDivision = new Button("/");
+        Button buttonResult = new Button("=");
+        Button buttonClear = new Button("clear");
 
         //Creating a Grid Pane
         GridPane gridPane = new GridPane();
@@ -49,26 +73,21 @@ public class LoginPage extends Application {
         gridPane.setAlignment(Pos.CENTER);
 
         //Arranging all the nodes in the grid
-        gridPane.add(text1, 0, 0);
-        gridPane.add(textField1, 1, 0);
-        gridPane.add(text2, 0, 1);
-        gridPane.add(textField2, 1, 1);
-        gridPane.add(button1, 0, 2);
-        gridPane.add(button2, 1, 2);
+        gridPane.add(expression, 0, 0, 5 ,1);
+        gridPane.add(textField1, 0, 1, 5, 1);
+        gridPane.add(buttonPlus, 0, 2);
+        gridPane.add(buttonMinus, 1, 2);
+        gridPane.add(buttonMultiplication, 2, 2);
+        gridPane.add(buttonDivision, 3, 2 );
+        gridPane.add(buttonResult, 4, 2);
+        gridPane.add(buttonClear, 5, 1);
 
-        //Styling nodes
-        button1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
-        button2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
-
-        text1.setStyle("-fx-font: normal bold 20px 'serif' ");
-        text2.setStyle("-fx-font: normal bold 20px 'serif' ");
-        gridPane.setStyle("-fx-background-color: BEIGE;");
 
         //Creating a scene object
         Scene scene = new Scene(gridPane);
 
         //Setting title to the Stage
-        stage.setTitle("CSS Example");
+        stage.setTitle("Кулькулятор");
 
         //Adding scene to the stage
         stage.setScene(scene);
@@ -77,6 +96,6 @@ public class LoginPage extends Application {
         stage.show();
     }
     public static void main(String args[]){
-        launch(args);
+        Application.launch(args);
     }
 }
